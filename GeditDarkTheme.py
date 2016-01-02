c1c72a4feeeea7e18c4b978868b9deb4e6b2458c
Franz Zapata <franz.zpt@gmail.com>
2016-01-01 21:23:05 -0700

    Relicense to Mozilla Public License

--- GeditDarkTheme.py
+++ GeditDarkTheme.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-# Copyright 2015 Franz Zapata
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy
-# of the License at http://www.apache.org/licenses/LICENSE-2.0
+# Copyright 2016 Franz Zapata
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from gi.repository import GObject, Gtk, Gedit
 
