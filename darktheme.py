57d0959e957407f84b209ab9b6176c2a32cc1b59
Fern Zapata <fern@zapata.cc>
2023-12-09 02:04:02 -0700

    Style: Format with Black

--- darktheme.py
+++ darktheme.py
@@ -3,20 +3,20 @@
 # or, at your option, any later version. If a copy of it was not
 # included with this file, see https://www.gnu.org/licenses/.
 
-from gi.repository import GObject, Gtk, Gedit
+from gi.repository import Gedit, GObject, Gtk
+
 
 class GeditDarkThemePlugin(GObject.Object, Gedit.AppActivatable):
+    app = GObject.Property(type=Gedit.App)
+    prop = "gtk-application-prefer-dark-theme"
 
-  app  = GObject.Property(type=Gedit.App)
-  prop = 'gtk-application-prefer-dark-theme'
+    def __init__(self):
+        super().__init__()
+        self.settings = Gtk.Settings.get_default()
 
-  def __init__(self):
-    super().__init__()
-    self.settings = Gtk.Settings.get_default()
+    def do_activate(self):
+        self._orig = self.settings.get_property(self.prop)
+        self.settings.set_property(self.prop, True)
 
-  def do_activate(self):
-    self._orig = self.settings.get_property(self.prop)
-    self.settings.set_property(self.prop, True)
-
-  def do_deactivate(self):
-    self.settings.set_property(self.prop, self._orig)
+    def do_deactivate(self):
+        self.settings.set_property(self.prop, self._orig)
